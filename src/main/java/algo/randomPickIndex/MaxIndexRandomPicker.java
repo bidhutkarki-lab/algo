6c6ebc83c4c5f0a81524de0300ae084b8f3b2312
bidhutkarki <bkarki@bppulsefleet.com>
2025-04-17 18:16:35 -0500

    Added pick random index variant

---
+++ src/main/java/algo/randomPickIndex/MaxIndexRandomPicker.java
@@ -0,0 +1,35 @@
+package algo.randomPickIndex;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+/*
+Time Complexity: O(n) - where n is the number of elements in the list.
+Space Complexity: O(m) - where m is the number of indices of the maximum value.
+ */
+public class MaxIndexRandomPicker {
+
+    public static int pickMaxIndex(List<Integer> nums) {
+        int max = Collections.max(nums); // First pass: get max value
+
+
+        List<Integer> maxIndices = new ArrayList<>();
+        for(int i=0; i<nums.size(); i++) {
+            if(nums.get(i) == max) {
+                maxIndices.add(i); // Second pass: collect indices of max value
+            }
+        }
+        Random rand = new Random();
+        return maxIndices.get(rand.nextInt(maxIndices.size())); // Pick one randomly
+    }
+
+    public static void main(String[] args) {
+        List<Integer> nums = Arrays.asList(5, 3, 9, 9, 2, 9);
+        int index = pickMaxIndex(nums);
+        System.out.println("Picked index: " + index + ", value = " + nums.get(index));
+    }
+
+}
---
+++ src/main/java/algo/randomPickIndex/ReservoirSampler.java
@@ -0,0 +1,46 @@
+package algo.randomPickIndex;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+/*
+You’re dealing with large data (or a stream) where storing everything isn’t practical.
+
+Time Complexity: O(n) where n is the number of input elements.
+
+Space Complexity: O(k). // this is the key, pick the k elements randomly without loading things in memory
+
+Probability correctness: Each element has equal chance of being chosen.
+ */
+public class ReservoirSampler {
+
+    public List<Integer> randomPickIndices(List<Integer> nums, int k) {
+        List<Integer> result = new ArrayList<>(k);
+        Random rand = new Random();
+
+        // Step 1: Fill the reservoir with the first k elements
+        for (int i = 0; i < k; i++) {
+            result.add(nums.get(i));
+        }
+
+        // Step 2: Replace elements with gradually decreasing probability
+        for (int i = k; i < nums.size(); i++) {
+            int r = rand.nextInt(i);  // Generates a number between 0 and i
+            if(r < k) {
+                result.set(r, nums.get(i));
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> nums = Arrays.asList(10, 20, 30, 40, 50, 60, 70);
+        int k = 3;
+
+        List<Integer> sample = new ReservoirSampler().randomPickIndices(nums, k);
+        System.out.println("Random Sample: " + sample);
+    }
+}
